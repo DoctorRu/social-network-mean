dffa23e2e01cccb078b9402e8e8f6797f690e0be
DoctorRu <vjblan@gmail.com>
2019-08-21 15:00:48 -0300

    Angular material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './post/post-create/post-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -13,6 +16,8 @@ import { PostCreateComponent } from './post/post-create/post-create.component';
   imports: [
     BrowserModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
